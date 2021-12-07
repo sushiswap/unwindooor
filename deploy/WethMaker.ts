e3e28bbb4027f181a71b0f9fc9b1317a5617f04c
gasper <gasper@sushi.com>
2021-12-07 22:09:18 +0100

    Add deploy scripts

---
+++ deploy/WethMaker.ts
@@ -0,0 +1,36 @@
+// import {  } from "hardhat-deploy";
+import { DeployFunction } from "hardhat-deploy/types";
+import { FACTORY_ADDRESS, WETH9_ADDRESS } from "@sushiswap/core-sdk";
+
+const deployFunction: DeployFunction = async ({
+  deployments,
+  getNamedAccounts,
+  getChainId
+}: any) => {
+
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const chainId = parseInt(await getChainId());
+
+  const owner = deployer;
+  const user = deployer;
+  const factory = FACTORY_ADDRESS[chainId];
+  const weth = WETH9_ADDRESS[chainId];
+
+  const { address } = await deploy("WethMaker", {
+    from: deployer,
+    args: [owner, user, factory, weth],
+  });
+
+  console.log(`Weth maker deployed to ${address}`);
+};
+
+deployFunction.skip = ({ getChainId }: any) =>
+  new Promise((resolve) => {
+    getChainId().then(chainId => {
+      return resolve(chainId === "1") // skip if on mainnet
+    })
+  });
+
+export default deployFunction;
